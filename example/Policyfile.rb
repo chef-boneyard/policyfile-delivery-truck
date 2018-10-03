11c2e1756254361c343b7c1c0d2403f1f97c408c
jtimberman <joshua@chef.io>
2018-10-03 14:14:25 -0700

    adjust example policyfile with some relevant examples
    Signed-off-by: Joshua Timberman <joshua@chef.io>

--- example/Policyfile.rb
+++ example/Policyfile.rb
@@ -2,10 +2,12 @@ name 'example'
 
 default_source :supermarket
 
-run_list 'apt', 'build-essential', 'packages', 'ntp', 'users::sysadmins', 'sudo'
+run_list 'build-essential', 'packages', 'ntp', 'sudo'
 
 default['authorization']['sudo']['passwordless'] = true
 default['authorization']['sudo']['users'] = ['ubuntu']
+
+default['packages-cookbook'] = ['git']
 
 # Something we can change when we want to modify the policyfile
 default['example']['some_number'] = 2
